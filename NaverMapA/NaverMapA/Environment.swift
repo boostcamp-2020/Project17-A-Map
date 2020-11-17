7bb7a0d872267b92429193a6d472b0019a24f333
hoonv <chaeefe@gmail.com>
2020-11-17 13:55:05 +0900

    [feat] remove nested enum for lint
    - 중첩된 enum 제거

--- NaverMapA/NaverMapA/Environment.swift
+++ NaverMapA/NaverMapA/Environment.swift
@@ -10,20 +10,16 @@ import Foundation
 public enum Environment {
 
     enum Keys {
-      enum Plist {
         static let clientId = "CLIENT_ID"
-      }
     }
-    
     private static let infoDictionary: [String: Any] = {
         guard let dict = Bundle.main.infoDictionary else {
             fatalError("Plist file not found")
         }
         return dict
     }()
-    
     static let clientId: String = {
-        guard let id = Environment.infoDictionary[Keys.Plist.clientId] as? String else {
+        guard let id = Environment.infoDictionary[Keys.clientId] as? String else {
             fatalError("API Key not set in plist for this environment")
         }
         return id
