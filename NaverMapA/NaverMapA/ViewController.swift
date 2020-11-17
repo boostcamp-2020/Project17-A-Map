1055125374fc5f68d02862fb6f77c603907309ca
hoonv <chaeefe@gmail.com>
2020-11-17 16:45:26 +0900

    [feat] make showAlert function for noticing client Id is nil
    - 타이틀 메세지 스타일 액션을 받으면 알림창을 보여주는 함수 생성해서 로직 분리

--- NaverMapA/NaverMapA/ViewController.swift
+++ NaverMapA/NaverMapA/ViewController.swift
@@ -18,14 +18,16 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         guard let _ = NMFAuthManager.shared().clientId else {
-            let alert = UIAlertController(title: "에러", message: "clientId 실종", preferredStyle: UIAlertController.Style.alert)
             let okAction = UIAlertAction(title: "OK", style: .destructive) { _ in
                 UIControl().sendAction(#selector(URLSessionTask.suspend), to: UIApplication.shared, for: nil)
             }
-            alert.addAction(okAction)
-            present(alert, animated: false, completion: nil)
+            showAlert(title: "에러", message: "ClientID가 없습니다.", preferredStyle: UIAlertController.Style.alert, action: okAction)
             return
         }
     }
-
+    private func showAlert(title: String?, message: String?, preferredStyle: UIAlertController.Style, action: UIAlertAction) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(action)
+        present(alert, animated: false, completion: nil)
+    }
 }
