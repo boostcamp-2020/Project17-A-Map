94a047993fb1d89c4641443154e7ab7a25422a16
kyungpyoda <qorghdl@naver.com>
2020-11-19 15:22:35 +0900

    [style] Place 불필요한 optional property 수정
    - null 값이 있는 imageUrl 속성만 optional 로 지정

--- NaverMapA/Place+CoreDataProperties.swift
+++ NaverMapA/Place+CoreDataProperties.swift
@@ -9,22 +9,21 @@
 import Foundation
 import CoreData
 
-
 extension Place {
 
     @nonobjc public class func fetchRequest() -> NSFetchRequest<Place> {
         return NSFetchRequest<Place>(entityName: "Place")
     }
 
-    @NSManaged public var category: [String]?
-    @NSManaged public var id: String?
+    @NSManaged public var category: String
+    @NSManaged public var id: String
     @NSManaged public var imageUrl: String?
     @NSManaged public var lat: Double
     @NSManaged public var lng: Double
-    @NSManaged public var name: String?
+    @NSManaged public var name: String
 
 }
 
-extension Place : Identifiable {
+extension Place: Identifiable {
 
 }
