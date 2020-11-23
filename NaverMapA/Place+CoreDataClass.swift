96fc3f181e7b52bc07716f8d73ea5e857cd0d72c
hoonv <chaeefe@gmail.com>
2020-11-23 23:18:20 +0900

    [feat] create configure for place with JsonPlace
    - JsonPlace로 configure

--- NaverMapA/Place+CoreDataClass.swift
+++ NaverMapA/Place+CoreDataClass.swift
@@ -12,4 +12,12 @@ import CoreData
 @objc(Place)
 public class Place: NSManagedObject {
 
+    func configure(json: JsonPlace) {
+        id = json.id
+        name = json.name
+        longitude = json.longitude
+        latitude = json.latitude
+        imageUrl = json.imageUrl
+        category = json.category
+    }
 }
