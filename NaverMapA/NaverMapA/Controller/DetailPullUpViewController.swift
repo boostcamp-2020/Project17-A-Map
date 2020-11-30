839f00dd0dbcb79b85212e886bbca408a8756198
kyungpyoda <qorghdl@naver.com>
2020-11-30 21:15:18 +0900

    [feat] Create DetailPullUpVC
    - 마커 상세정보를 보여주기 위한 PullUpVC

---
+++ NaverMapA/NaverMapA/Controller/DetailPullUpViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailPullUpViewController.swift
+//  NaverMapA
+//
+//  Created by 홍경표 on 2020/11/30.
+//
+
+import UIKit
+
+class DetailPullUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
