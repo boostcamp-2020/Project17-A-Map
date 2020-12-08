8442398b7aaaaa6861db82f0a9eb91c0db015508
hoonv <chaeefe@gmail.com>
2020-12-08 18:17:49 +0900

    [feat] create UserInfo for property wrapper
    - userinfo 생성

---
+++ NaverMapA/NaverMapA/Model/PropertyWrapper/UserInfo.swift
@@ -0,0 +1,28 @@
+//
+//  UserInfo.swift
+//  NaverMapA
+//
+//  Created by 채훈기 on 2020/12/08.
+//
+
+import Foundation
+
+@propertyWrapper
+struct UserInfo {
+    
+    private let key: String
+ 
+    var wrappedValue: String {
+        get { UserDefaults.standard.string(forKey: key) ?? "" }
+        set { UserDefaults.standard.set(newValue, forKey: key) }
+    }
+ 
+    init(key: String) {
+        self.key = key
+    }
+}
+
+struct InfoSetting {
+    @UserInfo(key: "algorithm") static var algorithm: String
+    @UserInfo(key: "animation") static var animation: String
+}
