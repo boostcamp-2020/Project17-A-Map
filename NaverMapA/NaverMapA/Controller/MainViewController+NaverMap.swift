af3ee6c34890cc3f7a136e3f9a03816c06f2e88d
hoonv <chaeefe@gmail.com>
2020-12-09 21:48:55 +0900

    [refactor] combine extension about NaverMap
    - 익스텐션 합침

--- NaverMapA/NaverMapA/Controller/MainViewController+NaverMap.swift
+++ NaverMapA/NaverMapA/Controller/MainViewController+NaverMap.swift
@@ -1,17 +1,27 @@
 //
-//  ViewController+NMFMapViewCameraDelegate.swift
+//  MainViewController+NMFOverlayImageDataSource.swift
 //  NaverMapA
 //
-//  Created by 박태희 on 2020/12/01.
+//  Created by 박태희 on 2020/11/26.
 //
 
 import Foundation
 import NMapsMap
 
+extension MainViewController: NMFOverlayImageDataSource {
+    
+    func view(with overlay: NMFOverlay) -> UIView {
+        let markerOverlay = overlay as? NMFMarker
+        let markerView = UIImageView(frame: CGRect(x: 0, y: 0, width: markerOverlay?.iconImage.imageWidth ?? 0, height: markerOverlay?.iconImage.imageHeight ?? 0))
+        markerView.image = markerOverlay?.iconImage.image.withTintColor(markerOverlay?.iconTintColor ?? .green)
+        return markerView
+    }
+}
+
 extension MainViewController: NMFMapViewCameraDelegate {
     
     func mapView(_ mapView: NMFMapView, cameraWillChangeByReason reason: Int, animated: Bool) {
-        animationLayer?.sublayers?.forEach {
+        animationLayer.sublayers?.forEach {
             $0.removeFromSuperlayer()
         }
         viewModel?.queue.cancelAllOperations()
@@ -39,5 +49,4 @@ extension MainViewController: NMFMapViewCameraDelegate {
             }
         }
     }
-    
 }
