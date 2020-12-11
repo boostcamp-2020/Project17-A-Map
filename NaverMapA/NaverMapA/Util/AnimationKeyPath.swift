6506f9e63bffe89378bf8d0a4f2a70c31509853c
hoonv <chaeefe@gmail.com>
2020-12-11 11:36:01 +0900

    [feat] create animation extension
    - basic 기본 구현 애니메이션 추가

---
+++ NaverMapA/NaverMapA/Util/AnimationKeyPath.swift
@@ -0,0 +1,38 @@
+//
+//  AnimationKeyPath.swift
+//  NaverMapA
+//
+//  Created by 채훈기 on 2020/12/10.
+//
+
+import Foundation
+
+enum AnimationKeyPath: String {
+    case anchorPoint
+    case backgroundColor
+    case backgroundFilters
+    case borderColor
+    case borderWidth
+    case bounds
+    case compositingFilter
+    case contents
+    case contentsRect
+    case cornerRadius
+    case doubleSided
+    case filters
+    case frame
+    case hidden
+    case mask
+    case masksToBounds
+    case opacity
+    case position
+    case shadowColor
+    case shadowOffset
+    case shadowOpacity
+    case shadowPath
+    case shadowRadius
+    case sublayers
+    case sublayerTransform
+    case transform
+    case zPosition
+}
