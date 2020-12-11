4d1a5887b9db26a405dda5f35eaa00bc978fbf17
Kim <skydevilk@gmail.com>
2020-12-12 08:24:28 +0900

    [feat] 카메라 이동 관련 리프노드 애니메이션 처리

--- NaverMapA/NaverMapA/Controller/MainViewController+NaverMap.swift
+++ NaverMapA/NaverMapA/Controller/MainViewController+NaverMap.swift
@@ -9,7 +9,6 @@ import Foundation
 import NMapsMap
 
 extension MainViewController: NMFOverlayImageDataSource {
-    
     func view(with overlay: NMFOverlay) -> UIView {
         let markerOverlay = overlay as? NMFMarker
         let markerView = UIImageView(frame: CGRect(x: 0, y: 0, width: markerOverlay?.iconImage.imageWidth ?? 0, height: markerOverlay?.iconImage.imageHeight ?? 0))
@@ -20,6 +19,28 @@ extension MainViewController: NMFOverlayImageDataSource {
 
 extension MainViewController: NMFMapViewCameraDelegate {
     
+    func mapView(_ mapView: NMFMapView, cameraIsChangingByReason reason: Int) {
+        guard let selected = naverMapView.selectedLeapMarker else {
+            flashAnimator.isAnimating = false
+            return
+        }
+        let coordBounds = naverMapView.coordBounds
+        if (selected.position.lat < coordBounds.southWestLat || selected.position.lng < coordBounds.southWestLng) || (selected.position.lat > coordBounds.northEastLat || selected.position.lng > coordBounds.northEastLng) {
+            if flashAnimator.isAnimating {
+                flashAnimator.isAnimating = false
+                flashAnimator.stop()
+            }
+        } else {
+            if !flashAnimator.isAnimating {
+                flashAnimator.isAnimating = true
+                flashAnimator.run()
+            } else {
+                naverMapView.selectedAnimationLayer?.position.x = naverMapView.mapView.layer.position.x - (mapView.projection.point(from: mapView.cameraPosition.target).x - mapView.projection.point(from: naverMapView.selectedAnimationStartCameraPosition).x)
+                naverMapView.selectedAnimationLayer?.position.y = naverMapView.mapView.layer.position.y - (mapView.projection.point(from: mapView.cameraPosition.target).y - mapView.projection.point(from: naverMapView.selectedAnimationStartCameraPosition).y)
+            }
+        }
+    }
+    
     func mapView(_ mapView: NMFMapView, cameraDidChangeByReason reason: Int, animated: Bool) {
         DispatchQueue.main.async {
             guard let viewModel = self.viewModel else { return }
@@ -28,11 +49,28 @@ extension MainViewController: NMFMapViewCameraDelegate {
             self.zoomLevelCheck = mapView.zoomLevel
             self.naverMapView.prevZoomLevel = mapView.zoomLevel
             if self.$zoomLevelCheck {
-                viewModel.updatePlacesAndAnimation(places: filtedPlaces, bounds: self.naverMapView.coordBounds)
+                viewModel.updatePlacesAndAnimation(places: filtedPlaces, bounds: self.naverMapView.coordBounds) { [weak self] in
+                    guard let self = self, let viewModel = self.viewModel, self.naverMapView.selectedLeapMarker != nil else { return }
+                    DispatchQueue.main.async {
+                        var findLeap = false
+                        for cluster in viewModel.beforeMarkers {
+                            if cluster.latitude == self.naverMapView.selectedLeapMarker?.position.lat && cluster.longitude == self.naverMapView.selectedLeapMarker?.position.lng {
+                                findLeap = true
+                                break
+                            }
+                        }
+                        if !findLeap {
+                            if self.flashAnimator.isAnimating {
+                                self.flashAnimator.isAnimating = false
+                                self.flashAnimator.stop()
+                                self.naverMapView.selectedLeapMarker = nil
+                            }
+                        }
+                    }
+                }
             }
         }
     }
-    
     func updateMapView() {
         DispatchQueue.main.async {
             guard let viewModel = self.viewModel else { return }
@@ -40,7 +78,9 @@ extension MainViewController: NMFMapViewCameraDelegate {
             let filtedPlaces = viewModel.fetchedPlaces(with: coordBounds)
             if self.naverMapView.prevZoomLevel != self.mapView.zoomLevel {
                 self.naverMapView.prevZoomLevel = self.mapView.zoomLevel
-                viewModel.updatePlacesAndAnimation(places: filtedPlaces, bounds: coordBounds)
+                viewModel.updatePlacesAndAnimation(places: filtedPlaces, bounds: coordBounds) {
+                    
+                }
             } else {
                 viewModel.updatePlaces(places: filtedPlaces, bounds: coordBounds)
             }
